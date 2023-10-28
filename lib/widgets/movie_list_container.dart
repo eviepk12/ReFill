bccdb1625d07f9ff93711cb63832992f89c42a9a
Zainal <eviepk12@gmail.com>
2023-10-28 21:33:38 +0700

    reworked movie lists

--- lib/widgets/movie_list_container.dart
+++ lib/widgets/movie_list_container.dart
@@ -6,13 +6,13 @@ import 'package:refill_app/widgets/section_title.dart';
 
 class MovieListContainer extends StatelessWidget {
   final String title;
-  final AsyncSnapshot snapshot;
 
   const MovieListContainer({
     super.key,
     required this.title,
     required this.snapshot,
   });
+  final AsyncSnapshot snapshot;
 
   @override
   Widget build(BuildContext context) {
@@ -24,34 +24,67 @@ class MovieListContainer extends StatelessWidget {
         //
         CarouselSlider.builder(
           options: CarouselOptions(height: 400),
-          // items: [1, 2, 3, 4, 5].map((i),
           itemCount: 15,
           itemBuilder: (context, itemIndex, pageViewIndex) {
             return Builder(
               builder: (BuildContext context) {
                 return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailScreen(
-                          movie: snapshot
-                              .data[itemIndex], // Removed 'const' from here
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                            movie: snapshot
+                                .data[itemIndex], // Removed 'const' from here
+                          ),
                         ),
-                      ),
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                          ),
+                          child: Container(
+                            color: Colours.accentColor.withOpacity(0.5),
+                            child: Image.network(
+                              '${ApiKeys.imageUrl}${snapshot.data[itemIndex].posterPath}',
+                              filterQuality: FilterQuality.high,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        // Container(
+                        //   color: Colours.accentColor.withOpacity(0.5),
+                        // )
+                      ],
+                    )
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 10),
+                    //   child: TransparentImageCard(
+                    //     width: MediaQuery.of(context).size.width,
+                    //     height: MediaQuery.of(context).size.height,
+                    //     imageProvider: NetworkImage(
+                    //         '${ApiKeys.imageUrl}${snapshot.data[itemIndex].posterPath}'),
+                    //     title: Text(
+                    //       '${snapshot.data[itemIndex].originalTitle}',
+                    //       style: const TextStyle(
+                    //         fontSize: 16,
+                    //         fontWeight: FontWeight.w600,
+                    //         color: Colors.white,
+                    //       ),
+                    //     ),
+                    //     description: Text(
+                    //       '\u{2B50}' '${snapshot.data[itemIndex].voteAverage}',
+                    //       style: const TextStyle(
+                    //           fontSize: 20, color: Colours.accentColor),
+                    //     ),
+                    //   ),
+                    // ),
                     );
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: const BoxDecoration(color: Colors.amber),
-                    child: Image.network(
-                      '${ApiKeys.imageUrl}${snapshot.data[itemIndex].posterPath}',
-                      filterQuality: FilterQuality.high,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
               },
             );
           },
